94a58eb97210a8d38ac5aa0e4ef18954e29f2b73
Nur Tahyudin <nurtahyuddin@gmail.com>
2025-12-16 02:34:08 +0700

    Update ssl

--- config/database.php
+++ config/database.php
@@ -58,11 +58,10 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-                // Tambahkan baris di bawah ini untuk melewati verifikasi file fisik jika lokasi sertifikat berbeda
+            'options' => [
+                PDO::MYSQL_ATTR_SSL_CA => null,
                 PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
-            ]) : [],
+            ],
         ],
 
         'mariadb' => [
