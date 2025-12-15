6428c53a2818b23801966b6bcb5b09df20bfd9be
Nur Tahyudin <nurtahyuddin@gmail.com>
2025-12-16 01:57:23 +0700

    Update ssl

--- config/database.php
+++ config/database.php
@@ -58,9 +58,9 @@ return [
             'prefix_indexes' => true,
             'strict' => true,
             'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                (PHP_VERSION_ID >= 80500 ? \Pdo\Mysql::ATTR_SSL_CA : \PDO::MYSQL_ATTR_SSL_CA) => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+            'options' => [
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', '/etc/ssl/certs/ca-certificates.crt'),
+            ],
         ],
 
         'mariadb' => [
