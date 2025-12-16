fe8922e15be8b9d6c14549db4748712230a43256
Nur Tahyudin <nurtahyuddin@gmail.com>
2025-12-16 10:11:34 +0700

    Update route

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Rute default web
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        "api_name" => "IURIS Legal API (Laravel Version)",
+        "status" => "Online",
+        "framework" => "Laravel",
+        "database" => "Cloud Aiven MySQL",
+        "endpoints" => [
+            "get_all_cases" => "/api/cases"
+        ],
+        "message" => "Gunakan /api/cases untuk mengakses data."
+    ]);
 });
